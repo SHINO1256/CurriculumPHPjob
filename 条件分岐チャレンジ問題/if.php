c30055e821538eef9c5dbc1e78a2415b89b50c3d
SHINO1256 <115599151+SHINO1256@users.noreply.github.com>
2022-11-03 04:53:19 +0900

    3章条件分岐チャレンジ問題(再提出)
    ご指摘をいただき26行目と29行目を書き直しました。よろしくお願いします。

--- 条件分岐チャレンジ問題/if.php
+++ 条件分岐チャレンジ問題/if.php
@@ -1,12 +1,32 @@
 <?php
 $name = "taro";
-$pass = "pass";
+$pass = "";
 
 if ($name == "taro" && $pass == "pass") {
     echo "ログイン成功です";
 } elseif ($name == "taro") {
     echo "パスワードが間違っています。";
 } elseif ($pass == "pass") {
+    echo "名前が間違っています。";
+} else {
+    echo "入力情報が間違っています";
+}
+echo "<br>";
+echo "<br>";
+echo "<br>";
+?> 
+
+<?php
+$name = "taro";
+$pass = "";
+
+if ($name == "taro" && $pass == "pass") {
+    echo "ログイン成功です";
+//ご指摘により修正
+} elseif ($name == "taro" && !$pass == "pass") {
+    echo "パスワードが間違っています。";
+//ご指摘により修正
+} elseif (!$name == "taro" && $pass == "pass")  {
     echo "名前が間違っています。";
 } else {
     echo "入力情報が間違っています";
